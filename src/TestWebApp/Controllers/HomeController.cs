db3f1a725ec38d244b2c5971596ad5d26d93174f
Gian Maria Ricci <alkampfer@nablasoft.com>
2019-03-26 22:08:53 +0100

    First checkin

---
+++ src/TestWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWebApp.Models;
+
+namespace TestWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new HomeModel();
+            model.Parameters.Add(new Parameter("Key1", ConfigurationManager.AppSettings["Key1"]));
+            model.Parameters.Add(new Parameter("Key2", ConfigurationManager.AppSettings["Key2"]));
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ src/TestWebApp/Models/HomeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestWebApp.Models
+{
+    public class HomeModel
+    {
+        public HomeModel()
+        {
+            Parameters = new List<Parameter>();
+        }
+
+        public IList<Parameter> Parameters { get; set; }
+    }
+
+    public class Parameter
+    {
+        public Parameter(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public String Key { get; set; }
+
+        public String Value { get; set; }
+    }
+}
