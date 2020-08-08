bde4ac2c73fdc3ab4bf5cbd6cc1fd7f90142e2eb
Gian Maria Ricci <alkampfer@nablasoft.com>
2020-08-08 12:12:31 +0200

    modified build

--- Build.ps1
+++ Build.ps1
@@ -1,4 +1,14 @@
-.\PreBuild.ps1 -configuration release
+<#
+.SYNOPSIS
+Build the entire project
+#>
+param (
+    [string] $webSiteOutDir = "release",
+    [string] $destinationDirectory = "artifacts",
+    [string] $buildConfiguration = "release"
+)
+
+.\PreBuild.ps1 -configuration $buildConfiguration
 
 Write-Host "Restoring nuget packages with nuget commandline"
 $nugetLocation = Get-NugetLocation
@@ -11,12 +21,12 @@ set-alias msb $msbuildLocation
 msb .\src\TestWebApp.sln /p:Configuration=release
 
 Write-Host "executing publishing of web site with msbuild with version"
-msb .\src\TestWebApp\TestWebApp.csproj /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:OutDir=".\release" /p:Configuration=release
+msb .\src\TestWebApp\TestWebApp.csproj /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:OutDir=".\$webSiteOutDir" /p:Configuration=$buildConfiguration
 
 Write-Host "Running nunit tests with console runner"
 $nunitConsoleRunner = GEt-NunitTestsConsoleRunner
 set-alias nunit "$nunitConsoleRunner"
 
-nunit .\src\TestWebApp.Tests\Bin\Release\TestWebApp.Tests.dll
+nunit ".\src\TestWebApp.Tests\Bin\$webSiteOutDir\TestWebApp.Tests.dll"
 
-.\PostBuild.ps1 -webSiteOutDir release -destinationDirectory .\artifacts -buildConfiguration release
+.\PostBuild.ps1 -webSiteOutDir $webSiteOutDir -destinationDirectory .\artifacts -buildConfiguration $buildConfiguration
